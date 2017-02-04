2340c69d134a972bafd6d001f423450e65c1a748
Alec Johnson <alecjohnson55@gmail.com>
2017-02-03 19:26:05 -0800

    Add API for a stack-queue

--- stack_queue.rb
+++ stack_queue.rb
@@ -5,4 +5,30 @@ class StackQueue
     @in_stack = MyStack.new
     @out_stack = MyStack.new
   end
+
+  def empty?
+    @in_stack.empty? && @out_stack.empty?
+  end
+
+  def size
+    @in_stack.size + @out_stack.size
+  end
+
+  def enqueue(val)
+    @in_stack.push(val)
+  end
+
+  def dequeue
+    queueify if @out_stack.empty?
+
+    @out_stack.pop
+
+  end
+
+  private
+
+  def queueify
+    @out_stack.push(@in_stack.pop) until @in_stack.empty?
+  end
+  
 end
